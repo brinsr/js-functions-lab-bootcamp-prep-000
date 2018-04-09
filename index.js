fea11bf33aca12b64bce2c1a9c7995bbca7c2c7e
Brindha S <brinsree@gmail.com>
2018-04-09 20:12:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // write your code below!
 function happyHolidays(){
-  return("Happy holidays!")
+  return("Happy holidays!");
+}
+function happyHolidays(nme){
+  return(`Happy holidays,${nme}!`);
+}
+function happyHolidayTo(hldy,nme){
+  return(`Happy ${hldy},`+` $(nme)!`);
+}
+function holidayCountdown(hldy,noDays){
+  return(`It's ${noDays)`+` $(hldy)!')
 }
